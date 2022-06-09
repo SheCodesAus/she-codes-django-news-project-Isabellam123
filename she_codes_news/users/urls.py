ec1a4dc73f4d34fa96747d539b77a4a6fe5111a7
Isabellam123 <isabella.mccarthy@tanda.co>
2022-06-09 20:49:54 +1000

    Django project

---
+++ she_codes_news/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import CreateAccountView, UserInterfaceView
+
+app_name = 'users'
+
+urlpatterns = [
+    path('create-account/', CreateAccountView.as_view(),name='createAccount'),
+    path('user-interface/', UserInterfaceView.as_view(),name='userInterface'),
+]
